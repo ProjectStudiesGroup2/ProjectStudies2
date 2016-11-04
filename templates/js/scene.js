8fc5f0d547ed9be165a85c5a0d73e3fb8d87954e
HYDROGEN\Dimitrios <mrhydro7k@gmail.com>
2016-11-04 03:47:32 +0200

    Tried some methods of implementing movement and other ways of importing scenes and assets. Follow up with finishing the key movement and scene layout.

--- templates/js/scene.js
+++ templates/js/scene.js
@@ -1,43 +1,157 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000); 
-                                                    //aspect ratio; width/height is a good default setup; near clipping frame; far clipping frame
-var light = new THREE.PointLight(0xFFFFFF);
-
-var geometry = new THREE.BoxGeometry(3, 1, 1);
-var material = new THREE.MeshLambertMaterial({color: 0x7a0c0c});
-var cube = new THREE.Mesh(geometry, material);
-var plane = new THREE.Mesh(geometry, material);
-// var controls;
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000);
+var spotLight = new THREE.SpotLight(0xffffff);
+spotLight.castShadow = true;
+spotLight.position.set(15, 30, 30);
+scene.add(spotLight);
+spotLight.shadowCameraNear = 1;
 var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-            
-    scene.add(cube);
-    scene.add(plane);
-    scene.add(light);
+renderer.setClearColor(0xdddddd);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMapEnabled = true;
+renderer.shadowMapSoft = true;
+document.body.appendChild(renderer.domElement);
 
-    camera.position.y = -50;
+var axis = new THREE.AxisHelper(10);
+scene.add (axis);
+var grid = new THREE.GridHelper(50, 5);
+var color = new THREE.Color("rgb(255,0,0)");
+grid.setColors(color, 0x000000);
+scene.add (grid);
 
-    plane.position.z = -3;
+var cubeGeometry = new THREE.BoxGeometry(5, 5, 5);
+var cubeMaterial = new THREE.MeshLambertMaterial({color: 0x7a0c0c});
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+var planeGeometry = new THREE.PlaneGeometry(100, 100, 100);
+var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
+var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-    light.position.x = 10;
-    light.position.y = 50;
-    light.position.z = 90;
+plane.rotation.x = -.5*Math.PI;
+plane.receiveShadow = true;
 
-    // controls = new THREE.OrbitControls(camera, renderer.domElement);
-    // controls.enableDamping = true;
-    // controls.dampingFactor = 0.25;
-    // controls.enableZoom = false;
+scene.add(plane);
+
+cube.position.x = 2.5;
+cube.position.y = 2.5;
+cube.position.z = 2.5;
+cube.castShadow = true;
+
+scene.add(cube);
+
+camera.position.x = 30;
+camera.position.y = 30;
+camera.position.z = 30;
+
+camera.lookAt(scene.position);
+
+document.addEventListener('keydown', keypress, false);
+function keypress(){
+    var speed = 0.1;
+
+    if(event.keyCode == 37){
+        cube.position.x += speed
+    }else if(event.keyCode == 39){
+        cube.position.x -= speed;
+    }else if(event.keyCode == 40){
+        cube.postion.z +=speed;
+    }else if(event.keyCode == 38){
+        cube.postion.z -=speed;
+    }
+}
 
 var render = function() {
-    requestAnimationFrame(render);
-    renderer.setClearColor(666666);
 
-    // controls.update();
+    // requestAnimationFrame(render);
+    // renderer.setClearColor(666666);
 
-    cube.rotation.x += 0.025;
-    cube.rotation.y += 0.05;
+    // cube.rotation.x += 0.025;
+    // cube.rotation.y += 0.05;
 
     renderer.render(scene, camera);
 }
-    render();
+render();
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<
+// animated scene
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000); 
+//                                                     //aspect ratio; width/height is a good default setup; near clipping frame; far clipping frame
+// var light = new THREE.PointLight(0xFFFFFF);
+
+// var geometry = new THREE.BoxGeometry(3, 1, 1);
+// var material = new THREE.MeshLambertMaterial({color: 0x7a0c0c});
+// var cube = new THREE.Mesh(geometry, material);
+// var plane = new THREE.Mesh(geometry, material);
+// // var controls;
+// var renderer = new THREE.WebGLRenderer();
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//     document.body.appendChild(renderer.domElement);
+            
+//     scene.add(cube);
+//     scene.add(plane);
+//     scene.add(light);
+
+//     camera.position.z = 50;
+
+//     plane.position.z = -3;
+
+//     light.position.x = 10;
+//     light.position.y = 50;
+//     light.position.z = 90;
+
+//     // controls = new THREE.OrbitControls(camera, renderer.domElement);
+//     // controls.enableDamping = true;
+//     // controls.dampingFactor = 0.25;
+//     // controls.enableZoom = false;
+
+// var render = function() {
+//     requestAnimationFrame(render);
+//     renderer.setClearColor(666666);
+
+//     // controls.update();
+
+//     cube.rotation.x += 0.025;
+//     cube.rotation.y += 0.05;
+
+//     renderer.render(scene, camera);
+// }
+//     render();
+
+// <<<<<<<<<<<<<<<<<<<<<<<
+// attemp the improting scene and assets
+// <<<<<<<<<<<<<<<<<<<<<<<
+
+// var camera, scene;
+
+// camera = loadScene.camera;
+// scene = loadScene.scene;
+
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// function createLoadScene(){
+//     var result = {
+//         scene: new THREE.Scene(),
+//         camera: new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
+
+//     };
+//     result.camera.position.z = 100;
+//     result.scene.add(result.camera);
+
+//     var geometry, material, light
+
+//     material = new THREE.MeshLambertMaterial({color: 0x7a0c0c});
+//     geometry = new THREE.BoxGeometry(3, 1, 1);
+
+//     light = new THREE.DirectionalLight( 0xffffff);
+//     result.scene.add(light);
+
+//     return result;
+// }
+// function render(){
+//     renderer.render(scene, camera);
+// }
+// render();
