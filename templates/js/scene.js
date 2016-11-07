a799cebceac69068c000eab6e4b52d415999fa7e
Roman <c1kuro00@students.oamk.fi>
2016-11-07 22:08:36 +0200

    Added moving of the ball by pressing W, A, S, D with a directing its path. Also ball and ground have a textures.

--- templates/js/scene.js
+++ templates/js/scene.js
@@ -19,7 +19,30 @@ var color = new THREE.Color("rgb(255,0,0)");
 grid.setColors(color, 0x000000);
 scene.add (grid);
 
+var keyboard = new KeyboardState();
+var clock = new THREE.Clock();
+
 var loader = new THREE.TGALoader();
+
+// FLOOR
+var img = new Image();
+img.crossOrigin = "anonymous";
+img.src = "img/grass.jpg"; 
+var floorTexture = new THREE.ImageUtils.loadTexture( img );
+floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
+floorTexture.repeat.set( 10, 10 );
+var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.position.y = -0.5;
+floor.rotation.x = Math.PI / 2;
+scene.add(floor);
+// SKYBOX/FOG
+var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
+var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+// scene.add(skyBox);
+scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
 
 //moving of a camera
 var SCREEN_WIDTH = window.innerWidth;
@@ -35,14 +58,28 @@ var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 THREE.ImageUtils.crossOrigin = '';
 
 // ball creation and texturing
-var texture = loader.load( 'ball.tga' );
+var sphere_texture, loader;
+var texture = loader.load( 'ball.png' );
 var geometry  = new THREE.SphereGeometry(3, 32, 32);
+
+// sphere_texture = new THREE.Texture();
+// loader = new THREE.ImageLoader();
+
+// loader.addEventListener('load', function (event){
+//     sphere_texture.image = event.content;
+//     sphere_texture.needsUpdate = true;
+// });
+
+// loader.load('ball.png');
+
 var material  = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('ball1.jpg') } );
+// var sphere_material = new THREE.MeshBasicMaterial({map: sphere_texture, overdraw: true});
 var sphere    = new THREE.Mesh(geometry, material);
+
+scene.add(sphere );
 
 //material.map  = THREE.ImageUtils.loadTexture('ball.png');
 //material.bumpScale = 0.05;
-
 
 plane.rotation.x = -.5*Math.PI;
 plane.receiveShadow = true;
@@ -54,46 +91,58 @@ sphere.position.y = 3;
 sphere.position.z = 0;
 sphere.castShadow = true;
 
-scene.add(sphere );
+
 
 camera = new THREE.PerspectiveCamera( 35, SCREEN_WIDTH / SCREEN_HEIGHT, 10, 2000 );
-camera.position.x = 30;
+camera.position.x = 70;
 camera.position.y = 30;
-camera.position.z = 100;
+camera.position.z = 0;
 
 camera.lookAt(scene.position);
 
-function onDocumentMouseMove( event ) {
-	mouseX = ( event.clientX - windowHalfX );
-	mouseY = ( event.clientY - windowHalfY );
-}
-
+// function onDocumentMouseMove( event ) {
+// 	mouseX = ( event.clientX - windowHalfX );
+// 	mouseY = ( event.clientY - windowHalfY );
+// }
 
 var render = function() {
 
     requestAnimationFrame(render);
 
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    // document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
-    document.addEventListener('keydown', function(event){
-    var speed = 0.01;
+    document.addEventListener('keydown', function(event){   
+        // var speed = 0.01; 
 
-        if(event.keyCode == 37){
-            sphere.position.x -= speed
-        }else if(event.keyCode == 39){
-            sphere.position.x += speed;
-        }else if(event.keyCode == 40){
-            sphere.position.z +=speed;
-        }else if(event.keyCode == 38){
-            sphere.position.z -=speed;
-        }
+        // if(event.keyCode == 37){
+        //     sphere.position.x -= speed
+        // }else if(event.keyCode == 39){
+        //     sphere.position.x += speed;
+        // }else if(event.keyCode == 40){
+        //     sphere.position.z +=speed;
+        // }else if(event.keyCode == 38){
+        //     sphere.position.z -=speed;
+        // }
+    keyboard.update();
 
-    console.log(sphere.position);
+	var moveDistance = 5 * clock.getDelta(); 
+
+	if ( keyboard.pressed("W") )
+		sphere.translateX( -moveDistance );
+		
+	if ( keyboard.pressed("S") )
+		sphere.translateX(  moveDistance );
+    
+    if ( keyboard.pressed("D") )
+		sphere.translateZ(  -moveDistance );
+    
+    if ( keyboard.pressed("A") )
+		sphere.translateZ(  moveDistance );        
     }, false);
 
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.position.y = THREE.Math.clamp( camera.position.y + ( - ( mouseY - 200 ) - camera.position.y ) * .05, 50, 1000 );
-	camera.lookAt( scene.position );
+    // camera.position.x += ( mouseX - camera.position.x ) * .05;
+	// camera.position.y = THREE.Math.clamp( camera.position.y + ( - ( mouseY - 20 ) - camera.position.y ) * .05, 50, 1000 );
+	// camera.lookAt( scene.position );
 
     renderer.render(scene, camera);        
 }
